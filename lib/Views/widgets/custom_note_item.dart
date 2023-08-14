60aa7d2a93321370ca40a3543bf5a41677430c36
Rajab-Alkahef <rajabalkahef3@gmail.com>
2023-08-14 15:33:57 +0300

    create custom note item

---
+++ lib/Views/widgets/custom_note_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Color(0xffffcd7d),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            trailing: const Icon(
+              Icons.delete,
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 18.0),
+            child: Text(
+              '14,aug,2023',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Views/widgets/notes_view_body.dart
+++ lib/Views/widgets/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,6 +17,7 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           customAppBar(),
+          NotesItem(),
         ],
       ),
     );
